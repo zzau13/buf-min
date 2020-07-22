63aa5783b5e00bab41e6ec78f88d466774f1cb6f
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-07-22 19:04:20 +0000

    Implement Buffer for Vec<u8>

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,62 @@ pub trait Buffer {
     unsafe fn buf_ptr(&mut self) -> *mut u8;
 }
 
+impl Buffer for Vec<u8> {
+    type Freeze = Vec<u8>;
+
+    #[inline]
+    fn with_capacity(capacity: usize) -> Self
+        where
+            Self: Sized,
+    {
+        Vec::with_capacity(capacity)
+    }
+
+    #[inline]
+    fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+
+    #[inline]
+    fn extend_from_slice(&mut self, src: &[u8]) {
+        Buffer::reserve(self, src.len());
+        unsafe {
+            debug_assert!(self.capacity() - self.len() >= src.len());
+            std::ptr::copy_nonoverlapping(src.as_ptr(), self.buf_ptr(), src.len());
+            Buffer::advance(self, src.len())
+        }
+    }
+
+    #[inline]
+    fn reserve(&mut self, additional: usize) {
+        debug_assert!(self.len() <= self.capacity());
+        if self.capacity().wrapping_sub(self.len()) < additional {
+            self.reserve(additional);
+        }
+    }
+
+    fn split(&mut self) -> Self where
+        Self: Sized {
+        self.split_off(self.len())
+    }
+
+    #[inline]
+    fn freeze(mut self) -> Self::Freeze {
+        self.truncate(self.len());
+        self
+    }
+
+    #[inline]
+    unsafe fn advance(&mut self, cnt: usize) {
+        self.set_len(self.len() + cnt);
+    }
+
+    #[inline]
+    unsafe fn buf_ptr(&mut self) -> *mut u8 {
+        self.as_mut_ptr().add(self.len())
+    }
+}
+
 #[cfg(feature = "bytes-buf")]
 impl Buffer for BytesMut {
     type Freeze = Bytes;
@@ -116,8 +172,33 @@ impl Buffer for BytesMut {
     }
 }
 
-#[cfg(all(test, feature = "bytes-buf"))]
+#[cfg(test)]
 mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let e = b"Hello world!";
+        let mut buf: Vec<u8> = Buffer::with_capacity(0);
+        Buffer::extend_from_slice(&mut buf, e);
+        assert_eq!(e, &Buffer::freeze(buf)[..]);
+
+        let mut buf: Vec<u8> = Buffer::with_capacity(124);
+        Buffer::extend_from_slice(&mut buf, e);
+        assert_eq!(e, &Buffer::freeze(buf)[..]);
+
+        let mut buf: Vec<u8> = Buffer::with_capacity(14);
+        Buffer::extend_from_slice(&mut buf, e);
+        let buf_c = Buffer::split(&mut buf);
+        assert_eq!(&buf_c[..], e);
+        assert_eq!(buf.len(), 0);
+        assert!(Buffer::is_empty(&buf));
+        assert_eq!(buf.capacity(), 2);
+    }
+}
+
+#[cfg(all(test, feature = "bytes-buf"))]
+mod test_bytes {
     use super::*;
 
     use bytes::BytesMut;
@@ -126,6 +207,10 @@ mod test {
     fn test() {
         let e = b"Hello world!";
         let mut buf: BytesMut = Buffer::with_capacity(0);
+        Buffer::extend_from_slice(&mut buf, e);
+        assert_eq!(e, &Buffer::freeze(buf)[..]);
+
+        let mut buf: BytesMut = Buffer::with_capacity(124);
         Buffer::extend_from_slice(&mut buf, e);
         assert_eq!(e, &Buffer::freeze(buf)[..]);
 
