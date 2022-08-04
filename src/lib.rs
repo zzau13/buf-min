fa5e4297f59985698417b5c01e889afc361f8215
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-08-04 23:02:45 +0200

    chore: bump MSRV to 1.57 and update features

--- src/lib.rs
+++ src/lib.rs
@@ -172,8 +172,7 @@ impl Buffer for String {
 }
 
 #[cfg(any(
-    feature = "bytes-buf-tokio2",
-    feature = "bytes-buf-tokio3",
+    feature = "bytes",
     feature = "ntex-bytes"
 ))]
 macro_rules! implement {
@@ -256,18 +255,10 @@ macro_rules! implement {
     };
 }
 
-#[cfg(feature = "bytes-buf-tokio2")]
-/// tokio/bytes@0.5 implementation and reexport
-pub mod t2 {
+#[cfg(feature = "bytes")]
+pub mod tokio_bytes {
     use super::*;
-    implement!(bytes_tokio2);
-}
-
-#[cfg(feature = "bytes-buf-tokio3")]
-/// tokio/bytes@1.0 implementation and reexport
-pub mod t3 {
-    use super::*;
-    implement!(bytes_tokio3);
+    implement!(bytes);
 }
 
 #[cfg(feature = "ntex-bytes")]
